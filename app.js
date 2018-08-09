96106e2b622aa72a72161ccc6441049554b2955e
whybazinga <aom9@mail.ru>
2018-08-09 23:48:47 +0300

    Added answer for moo message + added russian answeres

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
+
 const { token } = require('./top-secret');
+const util = require('./util');
 
 const bot = new Discord.Client();
 
@@ -8,8 +10,27 @@ bot.on('ready', () => {
 });
 
 bot.on('message', (msg) => {
-  if (msg.content.toLowerCase() === 'bisons') {
-    msg.channel.send('mooooo');
+  if (msg.author.username.toLowerCase() !== 'bisons bot') {
+    const msgInfo = util.checkContent(msg.content);
+    if (msgInfo.state) {
+      switch (msgInfo.lang) {
+        case 'ru':
+          msg.channel.send(util.getMoo('м', 'у'));
+          break;
+
+        case 'en':
+          msg.channel.send(util.getMoo('m', 'o'));
+          break;
+
+        case 'mixed':
+          msg.channel.send(util.getMoo('m', 'o', 5) + ' cyka blyat');
+          break;
+
+        default:
+          msg.channel.send('moo wat?');
+          break;
+      }
+    }
   }
 });
 
---
+++ util.js
@@ -0,0 +1,30 @@
+module.exports = (function () {
+  function getState(state = false, lang = null) {
+    return {
+      state,
+      lang,
+    };
+  }
+
+  const checkContent = (content) => {
+    const check = content.toLowerCase();
+    if ((check.includes('бизон') && check.includes('bison'))
+    || (check.includes('муу') && check.includes('moo'))) {
+      return getState(true, 'mixed');
+    }
+    if (check.includes('bison') || check.includes('moo')) {
+      return getState(true, 'en');
+    }
+    if (check.includes('бизон') || check.includes('муу')) {
+      return getState(true, 'ru');
+    }
+    return getState();
+  };
+
+  const getMoo = (m, o, count = 18) => m + (o + o).repeat(Math.floor(Math.random() * count + 1));
+
+  return {
+    checkContent,
+    getMoo,
+  };
+}());
