4e076a21d1a6143dc99c1ddbeef05e89a286d731
whybazinga <aom9@mail.ru>
2018-08-09 21:15:00 +0300

    Bot now sends mooooo to chat

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
-const {token} = require('./top-secret');
+const { token } = require('./top-secret');
 
 const bot = new Discord.Client();
 
 bot.on('ready', () => {
   console.log('Bot is up');
-})
+});
 
-bot.on('message',(msg) => {
-  if (msg.content.toLowerCase() === 'bisons'){
-    msg.channel.sendMessage('mooooo');
+bot.on('message', (msg) => {
+  if (msg.content.toLowerCase() === 'bisons') {
+    msg.channel.send('mooooo');
   }
-})
+});
 
 bot.login(token);
